697ec6209520960254799d300a321bbb79240ac1
dmercado <i202015515@cibertec.edu.pe>
2022-11-22 19:14:24 -0700

    1er commit del proyecto

---
+++ src/main/java/com/CL3/DawiCl3MercadoDanielApplication.java
@@ -0,0 +1,13 @@
+package com.CL3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3MercadoDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3MercadoDanielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CL3/DawiCl3MercadoDanielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CL3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3MercadoDanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
